1d7608f8107e3afd9a9a6e42a9512188788e4e79
Ranida01 <senmith01@gmail.com>
2025-12-02 22:29:02 +0530

    Completed MedicalRecord.js

---
+++ model/Admission.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const AdmissionSchema = new mongoose.Schema({
+    patient: { type: mongoose.Schema.Types.ObjectId, ref: "Patient", required: true },
+    bedNumber: { type: String, required: true },
+    ward: { type: String, required: true },
+    admissionDate: { type: Date, default: Date.now },
+    dischargeDate: { type: Date },
+    progressNotes: [{
+        note: String,
+        date: { type: Date, default: Date.now }
+    }]
+}, { timestamps: true });
+
+module.exports = mongoose.model("Admission", AdmissionSchema); 
